8353142d4857da679f879d62c50aa4712c8a6cb2
speranza0 <speranza0p@gmail.com>
2023-05-09 00:39:45 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편

---
+++ jpashop/src/main/java/JpaMain.java
@@ -0,0 +1,36 @@
+import jpabook.japshop.domain.Book;
+import jpabook.japshop.domain.Member;
+import jpabook.japshop.domain.Order;
+import jpabook.japshop.domain.OrderItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
+
+            em.persist(book);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
