2901f81e0334f7daf612d890e6508d5e818ca716
speranza0 <speranza0p@gmail.com>
2023-05-15 18:28:02 +0900

    자바 ORM 표준 JPA 프로그래밍 - 기본편

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -12,24 +13,36 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
             Member member1 = new Member();
-            member1.setUsername("관리자1");
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
 
-            em.flush();
-            em.clear();
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
-            String query = "select function('group_concat', m.username) From Member m";
-            List<String> result = em.createQuery(query, String.class)
-                            .getResultList();
-            
-            for (String s : result) {
-                System.out.println("s = " + s);
-            }
+            //FLUSH
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                            .executeUpdate();
+            em.clear();
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println("findMember.getAge() = " + findMember.getAge());
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
